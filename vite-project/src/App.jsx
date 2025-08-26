51bea4fea4b2e1a89772d8be99b7803550a4acba
Ayaan <desktop.ux@gmail.com>
2025-08-27 02:37:31 +0530

    First commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+const App = () => {
+
+
+  return (
+    <div className="w-screen h-screen bg-black">
+      <h1 className= "font-extrabold text-white">hello</h1>
+    </div>
+  )
+}
+
+export default App
