ef0c6e7db4871d98a31ceda6e0f4dc735e3187a1
Ayaan <desktop.ux@gmail.com>
2025-08-27 03:10:02 +0530

    Added profile card and edit form styling

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,21 @@
-
+import PublicCard from "./Components/ProfileCard"
+import EditForm from "./Components/EditForm"
+import UserStats from "./Components/UserStats"
 
 const App = () => {
 
 
   return (
-    <div className="w-screen h-screen bg-black">
-      <h1 className= "font-extrabold text-white">hello</h1>
+    <div className="w-screen h-screen bg-gray-100 py-8">
+        <div className="max-w-lg mx-auto px-4">
+           <h1 className="text-3xl font-bold text-center mb-8 text-gray-800">
+          User Profile Manager
+        </h1>
+
+        <PublicCard/>
+
+        <EditForm/>
+        </div>
     </div>
   )
 }
--- vite-project/src/Components/EditForm.jsx
+++ vite-project/src/Components/EditForm.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
 const EditForm = () => {
-  return (
-    <div>EditForm</div>
-  )
+    return (
+        <div className='bg-white rounded-lg shadow-md p-6 mb-6"'>
+            <h1 className='text-lg font-bold mb-4 text-gray-800'>Edit Profile</h1>
+
+            <div className='space-y-4'>
+                <div>
+                    <label className='block text-sm font-medium text-gray-700 mb-1.5'>Name</label>
+                    <input type="text" placeholder='Enter Your Name' className='w-full px-3 py-1 border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500' />
+                </div>
+                <div>
+                    <label className='block text-sm font-medium text-gray-700 mb-1.5'>Age</label>
+                    <input type="number" placeholder='Enter Your Age' className='w-full px-3 py-1 border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500' />
+                </div>
+                <div>
+                    <label className='block text-sm font-medium text-gray-700 mb-1.5'>Email</label>
+                    <input type="email" placeholder='Enter Your Email Address' className='w-full px-3 py-1 border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500' />
+                </div>
+            </div>
+            <div className='flex gap-3 mt-6'>
+                <button className='flex-1 bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 transition-colors'>Save Changes</button>
+                <button className='flex-1 bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-600 transition-colors'>Cancel</button>
+            </div>
+        </div>
+    )
 }
 
 export default EditForm
